91ea7fadc40f7410666def6fde0daac165022826
guiiivale <guigato62@gmail.com>
2024-01-13 17:27:52 -0300

    Created function to get ingame data and update

--- app/Console/Commands/TestCommand.php
+++ app/Console/Commands/TestCommand.php
@@ -11,6 +11,8 @@ use Discord\Parts\WebSockets\TypingStart;
 use Discord\WebSockets\Intents;
 use Discord\WebSockets\Event;
 use App\Services\OpGGService;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Log;
 
 class TestCommand extends Command
 {
@@ -50,102 +52,166 @@ class TestCommand extends Command
         $discord->on('ready', function (Discord $discord) {
             echo "Bot is ready!", PHP_EOL;
         });
-
-        $discord->on(Event::MESSAGE_CREATE, function (Message $message, Discord $discord) {
-            if ($message->author->bot) {
-                return;
-            }
-
-            $lastChar = substr($message->content, -1);
-            $penultimateChar = substr($message->content, -2, 1);
-
-            if ($lastChar === '?' || $penultimateChar === '?') {
-                $message->channel->sendMessage('Se quiser sim mano');
-            }
-
-            if (stripos($message->content, 'guizap') !== false) {
-                $message->react('🖕');
-            }
-
-            if (strtolower(substr($message->content, 0, 7)) == 'player:') {
-
-                $formattedName = $this->opGGService->transformPlayerName($message->content);
-
-                $data = $this->opGGService->obterDadosOpGG($formattedName);
-
-                if(!isset($data['pageProps']['data']['level'])) {
-                    $message->channel->sendMessage('Não encontrei nada, tente novamente, tente como o exemplo: soul#gule');
+        try {
+            $discord->on(Event::MESSAGE_CREATE, function (Message $message, Discord $discord) {
+                if ($message->author->bot) {
                     return;
                 }
+    
+                $lastChar = substr($message->content, -1);
+                $penultimateChar = substr($message->content, -2, 1);
+    
+                if ($lastChar === '?' || $penultimateChar === '?') {
+                    $message->channel->sendMessage('Se quiser sim mano');
+                }
+    
+                if (stripos($message->content, 'guizap') !== false) {
+                    $message->react('🖕');
+                }
+    
+                if (strtolower(substr($message->content, 0, 7)) == 'update:') {
+                    $formattedName = $this->opGGService->transformPlayerName($message->content);
+    
+                    $data = $this->opGGService->updateData($formattedName);
+    
+                    if ($data['status'] != '200') {
+                        $message->channel->sendMessage('Erro ao atualizar!');
+                        $message->channel->sendMessage('Motivo do erro: ' . $data['data']['message']);
+                        if (isset($data['data']['renewable_at'])) {
+                            $dataHoraCarbon = Carbon::parse($data['data']['renewable_at']);
+    
+                            $dataHoraCarbon->setTimezone('America/Sao_Paulo');
+    
+                            $dataHoraFormatada = $dataHoraCarbon->format('d-m-Y H:i:s');
+    
+                            $message->channel->sendMessage('Tente novamente em: ' . $dataHoraFormatada);
+                        }
+                        return;
+                    }
+    
+                    $message->channel->sendMessage('Atualizado com sucesso!');
+                }
+    
+                if (strtolower(substr($message->content, 0, 7)) == 'player:') {
+    
+                    $formattedName = $this->opGGService->transformPlayerName($message->content);
+    
+                    $data = $this->opGGService->getDataOpGG($formattedName);
+    
+                    if (!isset($data['pageProps']['data']['level'])) {
+                        $message->channel->sendMessage('Não encontrei nada, tente novamente, tente como o exemplo: soul#gule');
+                        return;
+                    }
+    
+                    if (isset($data['pageProps']['statusCode']) && $data['pageProps']['statusCode'] == '404') {
+                        $message->channel->sendMessage('Não encontrei nada, tente novamente, tente como o exemplo: soul#gule');
+                        return;
+                    }
+    
+                    if ($data) {
+                        $previousSeasons = $data['pageProps']['data']['previous_seasons'];
+                        $currentLevel = $data['pageProps']['data']['level'];
+                        $gameName = $data['pageProps']['data']['game_name'];
+    
+                        $messageString = "Previous Season Elos:\n";
+    
+                        foreach ($previousSeasons as $season) {
+                            $tierInfo = $season['tier_info'];
+                            $createdAt = date('Y-m-d', strtotime($season['created_at']));
+    
+                            $eloInfo = "{$tierInfo['tier']} {$tierInfo['division']} - LP: {$tierInfo['lp']}";
+    
+                            $messageString .= "- Season {$season['season_id']} ({$createdAt}): {$eloInfo}\n";
+                        }
+                        $currentLevelMessage = "Game name: {$gameName} \nCurrent Level: {$currentLevel}\n";
+                        $leagueStats = $data['pageProps']['data']['league_stats'];
+    
+                        $leagueStatsMessage = "League Stats:\n";
+    
+                        foreach ($leagueStats as $stats) {
+                            $queueInfo = $stats['queue_info'];
+                            $tierInfo = $stats['tier_info'];
+    
+                            $win = $stats['win'] ? $stats['win'] : '0';
+                            $lose = $stats['lose'] ? $stats['lose'] : '0';
+    
+                            $formattedTierInfo = $this->formatTierInfo($tierInfo);
+    
+                            $statInfo = "Queue: {$queueInfo['queue_translate']}\n";
+                            $statInfo .= "Tier Info: {$formattedTierInfo}\n";
+                            $statInfo .= "Wins: {$win}\n";
+                            $statInfo .= "Losses: {$lose}\n";
+    
+                            $leagueStatsMessage .= "{$statInfo}\n";
+                        }
+    
+                        $message->channel->sendMessage($currentLevelMessage . "\n" . "\n" . "\n" . "\n");
+                        $message->channel->sendMessage($leagueStatsMessage . "\n" . "\n" . "\n" . "\n");
+                        $message->channel->sendMessage($messageString . "\n" . "\n" . "\n" . "\n");
+                        $profileImageUrl = $data['pageProps']['data']['profile_image_url'];
+    
+                        $message->channel->sendMessage('Summoner icon: ' . "\n");
+                        $message->channel->sendMessage($profileImageUrl);
+                    }
 
-                if(isset($data['pageProps']['statusCode']) && $data['pageProps']['statusCode'] == '404') {
-                    $message->channel->sendMessage('Não encontrei nada, tente novamente, tente como o exemplo: soul#gule');
+                    $maxAttempts = 5;
+                    $attempts = 0;
+                    $success = false;
+                    
+                    while ($attempts < $maxAttempts && !$success) {
+                        try {
+                            $ingameData = $this->opGGService->ingameData($formattedName);
+
+                            $ingameData = $ingameData['data'];
+
+                            $reply = "Match details:\n\n";
+                            $reply .= "**Match ID:** {$ingameData['game_id']}\n";
+                            $reply .= "**Game Type:** {$ingameData['queue_info']['queue_translate']}\n";
+                            $reply .= "**Map:** {$ingameData['game_map']}\n\n";
+                            $reply .= "**Participants:**\n";
+                    
+                            foreach ($ingameData['participants'] as $participant) {
+                                $reply .= "----------------------\n";
+                                $reply .= "**Name:** {$participant['summoner']['name']}\n";
+                                $reply .= "**Team side:** {$participant['team_key']}\n";
+                                $reply .= "**Position:** {$participant['position']}\n";
+                                $reply .= "**Level:** {$participant['summoner']['level']}\n";
+                            }
+                    
+                            $message->channel->sendMessage($reply);
+                            $success = true;
+                        } catch (\Exception $e) {
+                            $attempts++;
+                            sleep(1);
+                            Log::error($e->getMessage());
+                        }
+                    }
+                    
+                    if (!$success) {
+                        $message->channel->sendMessage('Not in game.');
+                    }
+                }
+            });
+    
+            $discord->on(Event::TYPING_START, function (TypingStart $typing, Discord $discord) {
+                if ($typing->user->bot) {
                     return;
                 }
-
-                if ($data) {
-                    $previousSeasons = $data['pageProps']['data']['previous_seasons'];
-                    $currentLevel = $data['pageProps']['data']['level'];
-                    $gameName = $data['pageProps']['data']['game_name'];
-
-                    $messageString = "Previous Season Elos:\n";
-
-                    foreach ($previousSeasons as $season) {
-                        $tierInfo = $season['tier_info'];
-                        $createdAt = date('Y-m-d', strtotime($season['created_at']));
-
-                        $eloInfo = "{$tierInfo['tier']} {$tierInfo['division']} - LP: {$tierInfo['lp']}";
-
-                        $messageString .= "- Season {$season['season_id']} ({$createdAt}): {$eloInfo}\n";
-                    }
-                    $currentLevelMessage = "Game name: {$gameName} \nCurrent Level: {$currentLevel}\n";
-                    $leagueStats = $data['pageProps']['data']['league_stats'];
-
-                    $leagueStatsMessage = "League Stats:\n";
-
-                    foreach ($leagueStats as $stats) {
-                        $queueInfo = $stats['queue_info'];
-                        $tierInfo = $stats['tier_info'];
-
-                        $win = $stats['win'] ? $stats['win'] : '0';
-                        $lose = $stats['lose'] ? $stats['lose'] : '0';
-
-                        $formattedTierInfo = $this->formatTierInfo($tierInfo);
-
-                        $statInfo = "Queue: {$queueInfo['queue_translate']}\n";
-                        $statInfo .= "Tier Info: {$formattedTierInfo}\n";
-                        $statInfo .= "Wins: {$win}\n";
-                        $statInfo .= "Losses: {$lose}\n";
-
-                        $leagueStatsMessage .= "{$statInfo}\n";
-                    }
-
-                    $message->channel->sendMessage($currentLevelMessage . "\n" . "\n". "\n" . "\n");
-                    $message->channel->sendMessage($leagueStatsMessage . "\n" . "\n". "\n" . "\n");
-                    $message->channel->sendMessage($messageString . "\n" . "\n". "\n" . "\n");
-                    $profileImageUrl = $data['pageProps']['data']['profile_image_url'];
-
-                    $message->channel->sendMessage('Summoner icon: ' . "\n");
-                    $message->channel->sendMessage($profileImageUrl);
+    
+                $typing->channel->sendMessage($typing->user->username . ' está digitando... corram.');
+            });
+    
+            $discord->on(Event::MESSAGE_DELETE, function (object $message, Discord $discord) {
+                if ($message instanceof Message) {
+                    $message->channel->sendMessage('Mensagem de ' . $message->author->username . ' deletada: ' . PHP_EOL . '"' . $message->content . '"');
                 }
-            }
-        });
-
-        $discord->on(Event::TYPING_START, function (TypingStart $typing, Discord $discord) {
-            if ($typing->user->bot) {
-                return;
-            }
-
-            $typing->channel->sendMessage($typing->user->username . ' está digitando... corram.');
-        });
-
-        $discord->on(Event::MESSAGE_DELETE, function (object $message, Discord $discord) {
-            if ($message instanceof Message) {
-                $message->channel->sendMessage('Mensagem de ' . $message->author->username . ' deletada: ' . PHP_EOL . '"' . $message->content . '"');
-            }
-        });
-
-        $discord->run();
+            });
+    
+            $discord->run();
+        }
+        catch (\Exception $e) {
+            echo $e->getMessage();
+        }
     }
 
     function formatTierInfo($tierInfo)
--- app/Services/OpGGService.php
+++ app/Services/OpGGService.php
@@ -4,30 +4,11 @@ namespace App\Services;
 
 class OpGGService
 {
-    public function obterDadosOpGG($summonerName)
+    public function getDataOpGG($summonerName)
     {
         $url = "https://www.op.gg/_next/data/hNkxrToTx8MW15R_8ziWW/en_US/summoners/br/{$summonerName}.json?region=br";
 
-        $curl = curl_init();
-
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => $url,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => '',
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => 'GET',
-        ));
-
-        $response = curl_exec($curl);
-
-        curl_close($curl);
-
-        $dados = json_decode($response, true);
-
-        return $dados;
+        return $this->curl($url);
     }
 
     public function transformPlayerName($playerName)
@@ -37,5 +18,47 @@ class OpGGService
         $formattedName = strtolower(str_replace([' ', '#'], ['%20', '-'], $playerName));
 
         return $formattedName;
+    }
+
+    public function updateData($summonerName)
+    {
+        $id = $this->getDataOpGG($summonerName)['pageProps']['data']['summoner_id'];
+
+        $url = "https://op.gg/api/v1.0/internal/bypass/summoners/br/{$id}/renewal";
+
+        return $this->curl($url, 'POST');
+    }
+
+    protected function curl($url, $method = 'GET', $data = [])
+    {
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_CUSTOMREQUEST => $method,
+            CURLOPT_POSTFIELDS => $data,
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+
+        $data = json_decode($response, true);
+
+        return $data;
+    }
+
+    public function ingameData($summonerName)
+    {
+        $id = $this->getDataOpGG($summonerName)['pageProps']['data']['summoner_id'];
+
+        $url = "https://op.gg/api/v1.0/internal/bypass/spectates/br/{$id}?hl=en_US";
+
+        return $this->curl($url);
     }
 }
